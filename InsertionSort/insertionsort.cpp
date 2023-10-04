69aad03109693ad7e8e9641dfe335c34d7920cdf
Your Name <you@example.com>
2023-10-04 14:06:09 +0530

    feat(insertionsort): :rocket:

---
+++ InsertionSort/insertionsort.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+void insertionSort(int arr[], int n){
+    for(int i=0;i<n;i++){
+        int key = arr[i];
+        int j = i-1;
+        while(j>=0 && arr[j]>key){
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1]=key;
+    }
+}
+
+void printArray(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
+
+int main(){
+    int n;
+    cout<<"Enter the size of array: ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the elements of array: ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<"\nArray before sorting: ";
+    printArray(arr,n);
+    insertionSort(arr,n);
+    cout<<"\nArray after sorting: ";
+    printArray(arr,n);
+    return 0;
+}
