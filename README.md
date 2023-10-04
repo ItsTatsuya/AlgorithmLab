219d89dc032614eb18296e8996d64bfd9925a1c0
Your Name <you@example.com>
2023-10-04 14:30:54 +0530

    build: :white_check_mark:

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 - - [x] [**Linear Search**](https://github.com/ItsTatsuya/AlgorithmLab/tree/main/LinearSearch)
 - - [x] [**Bubble Sort**](https://github.com/ItsTatsuya/AlgorithmLab/tree/main/BubbleSort)
 - - [x] [**Insertion Sort**](https://github.com/ItsTatsuya/AlgorithmLab/tree/main/InsertionSort)
+- - [x] [**Selection Sort**](https://github.com/ItsTatsuya/AlgorithmLab/tree/main/SelectionSort)
---
+++ SelectionSort/selectionsort.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+void selectionSort(int arr[], int n){
+    for(int i=0;i<n-1;i++){
+        int min = i;
+        for(int j=i+1;j<n;j++){
+            if(arr[j]<arr[min]){
+                min = j;
+            }
+        }
+        int temp = arr[min];
+        arr[min] = arr[i];
+        arr[i] = temp;
+    }
+}
+
+void printArray(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
+
+int main(){
+    int n;
+    cout<<"Enter the size of array: ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the elements of array: ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<"\nArray before sorting: ";
+    printArray(arr,n);
+    selectionSort(arr,n);
+    cout<<"\nArray after sorting: ";
+    printArray(arr,n);
+    return 0;
+}
