cc348de8596ddcf206c4183dcf11bd1ef4f06a33
Your Name <you@example.com>
2023-12-13 11:32:13 +0530

    dijakstras done

--- Lab-Cycle-2/DijkstraAlgorithm/code.cpp
+++ Lab-Cycle-2/DijkstraAlgorithm/code.cpp
@@ -1,71 +1,56 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <climits>
+//Dijakstra's Algorithm
 
+#include <iostream>
 using namespace std;
 
-// Function to implement Dijkstra's algorithm
-void dijkstra(vector<vector<pair<int, int>>>& graph, int source) {
-    int n = graph.size();
-    vector<int> dist(n, INT_MAX);
-    vector<bool> visited(n, false);
-
-    dist[source] = 0;
-
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    pq.push({0, source});
-
-    while (!pq.empty()) {
-        int u = pq.top().second;
-        pq.pop();
-
-        if (visited[u]) {
-            continue;
+int Dijakstra(int n, int cost[10][10], int source, int dest)
+{
+    int dist[10], visited[10], min, u, v;
+    for (int i = 1; i <= n; i++)
+    {
+        dist[i] = cost[source][i];
+        visited[i] = 0;
+    }
+    visited[source] = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        min = 999;
+        for (int j = 1; j <= n; j++)
+        {
+            if (min > dist[j] && visited[j] == 0)
+            {
+                min = dist[j];
+                u = j;
+            }
         }
-
-        visited[u] = true;
-
-        for (auto& neighbor : graph[u]) {
-            int v = neighbor.first;
-            int weight = neighbor.second;
-
-            if (dist[u] + weight < dist[v]) {
-                dist[v] = dist[u] + weight;
-                pq.push({dist[v], v});
+        visited[u] = 1;
+        for (v = 1; v <= n; v++)
+        {
+            if (visited[v] == 0)
+            {
+                if (dist[v] > dist[u] + cost[u][v])
+                    dist[v] = dist[u] + cost[u][v];
             }
         }
     }
-
-    // Print the shortest distances from the source
-    cout << "Shortest distances from source " << source << ":\n";
-    for (int i = 0; i < n; i++) {
-        cout << "Vertex " << i << ": " << dist[i] << "\n";
-    }
+    return dist[dest];
 }
 
-int main() {
-    int n, m;
+int main()
+{
+    int n, cost[10][10], source, dest, dist;
     cout << "Enter the number of vertices: ";
     cin >> n;
-    cout << "Enter the number of edges: ";
-    cin >> m;
-
-    vector<vector<pair<int, int>>> graph(n);
-
-    cout << "Enter the edges and their weights:\n";
-    for (int i = 0; i < m; i++) {
-        int u, v, weight;
-        cin >> u >> v >> weight;
-        graph[u].push_back({v, weight});
-        graph[v].push_back({u, weight});
-    }
-
-    int source;
+    cout << "Enter the cost matrix: ";
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= n; j++)
+            cin >> cost[i][j];
     cout << "Enter the source vertex: ";
     cin >> source;
-
-    dijkstra(graph, source);
-
+    cout << "Enter the destination vertex: ";
+    cin >> dest;
+    dist = Dijakstra(n, cost, source, dest);
+    cout << "The shortest distance between " << source << " and " << dest << " is " << dist << endl;
     return 0;
 }
+
